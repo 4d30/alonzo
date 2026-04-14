1cf276b01bf8b2c5f3920193dc0a75741145d62e
Joe M <45021583+4d30@users.noreply.github.com>
2026-04-14 16:59:45 -0400

    Initial commit

---
+++ church.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from functools import partial, reduce
+from itertools import repeat
+
+def safe_call(f, x):
+    return f(x) if x is not None else None
+
+def apply(x, f):
+    return safe_call(f, x)
+
+def sequential(funcs):
+    return partial(reduce, apply, funcs)
+
+def juxt(funcs, x):
+    def inner(x):
+        return tuple(map(apply, repeat(x), funcs))
+    return safe_call(inner, x)
+
+def parallel(funcs):
+    return partial(juxt, funcs)
+
+def branch(pred, f, g):
+    def h(x):
+        def choose(x):
+            if pred(x):
+                return f(x)
+            return g(x)
+        return safe_call(choose, x)
+    return h
+
+#from operator import call
+#def juxt(funcs, x):
+#    if x is not None:
+#        return tuple(map(call, funcs, repeat(x)))
+#    return None
